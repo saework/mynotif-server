2b201aba0c3cf9132474a2cb4a2ef507061e9b24
saework <saework@ya.ru>
2021-01-08 00:54:34 +0500

    работа с node-cron

--- app.js
+++ app.js
@@ -1,12 +1,119 @@
 const express = require("express");
 const cron = require('node-cron');
+const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const app = express();
 
-cron.schedule('* * * * *', () => {
-	console.log('running a task every minute');
-  });
+const TIMEZONE = "Asia/Yekaterinburg";
+//const TIMEZONE = "Europe/Moscow";
+
+const cronTasks={};
+
+const cronTaskParamsArr = [
+	{cronTaskName: "test_test_1",
+	 cronTaskTime: "08 00 * * *",
+	 emailAddress: "saework@ya.ru",
+	 emailText: "задача 1"
+	},
+	{cronTaskName: "test_test_2",
+	 cronTaskTime: "09 00 * * *",
+	 emailAddress: "saework@ya.ru",
+	 emailText: "задача 2"
+	},
+];
+
+async function sendEmail(text, address){
+	let transporter = nodemailer.createTransport({
+	host: "smtp.yandex.ru",
+	port: 465,
+	secure: true,
+	auth: {
+		user: "my-notif@yandex.ru",
+		pass: "MM24686421x" 
+	}
+	});
+	// send email
+	let emailInfo = await transporter.sendMail({
+		from: "my-notif@yandex.ru", 
+		to: address, 
+		subject: "Уведомление mynotif.ru", 
+		text: text, 
+		// html: "<b>Текст</b>" 
+	});
+	const now = new Date();
+	console.log(`Email - sent - ${emailInfo.messageId} - ${address} - ${text} - ${now}`);
+	//console.log("Email sent: %s", info.messageId);
+ }
+
+ let startCronTasks = (cronTaskParamsArr) => {
+	cronTaskParamsArr.forEach(function(cronTaskParamsObj) 
+	{
+		const cronTaskName=cronTaskParamsObj.cronTaskName;
+		const cronTaskTime=cronTaskParamsObj.cronTaskTime;
+		const emailAddress=cronTaskParamsObj.emailAddress;
+		const emailText=cronTaskParamsObj.emailText;
+		const now = new Date();
+		cronTasks[cronTaskName] = cron.schedule(cronTaskTime, () => 
+		{
+		  console.log(`Task - start - ${cronTaskName} - ${now}`);
+		  sendEmail(emailText, emailAddress).catch(console.error);
+		}, {
+		  scheduled: true,
+		  timezone: TIMEZONE
+		});
+		console.log(`Task - start waiting - ${cronTaskName} - ${now}`);
+		cronTasks[cronTaskName].start();	
+	});
+ };
+
+ let stopCronTasks = (cronTasks) => {
+	 //console.log(cronTasks);
+	 for (let key in cronTasks) {
+		const now = new Date();
+		console.log(`Task - destroy - ${key} - ${now}`);
+		cronTasks[key].destroy();
+	  }
+
+	//  for (let key in cronTasks) {
+	// 	console.log(key, ':', cronTasks[key]);
+	//   }
+	// cronTasks.forEach(function(cronTask) 
+	// {
+	// 	console.log(`Task - destroy - ${cronTask} - ${now}`);
+	// 	cronTask.destroy();
+	// });
+ };
+
+//   // останавливаем все cron tasks, чтобы запустить с учетом изменений
+//   cron.schedule('57 23 * * *', () => 
+//   {
+// 	console.log('tasks: stoping all');
+// 	}, {
+// 	scheduled: true,
+// 	timezone: "Asia/Yekaterinburg"
+// 	//timezone: "Europe/Moscow"
+//   });
+
+//   // запускаем все cron tasks, чтобы с учетом изменений + на случай падения сервера
+//   cron.schedule('0 0 * * *', () =>
+//   {
+// 	console.log('tasks: starting all');
+// 	}, {
+// 	scheduled: true,
+// 	timezone: "Asia/Yekaterinburg"
+// 	//timezone: "Europe/Moscow"
+//   });
+
+// //cron.schedule('*/2 * * * *', () => {
+// 	cron.schedule('03 21 07 01 *', () => {
+// 	console.log('running a task');
+// 	sendEmail("Приветик", "saework@ya.ru").catch(console.error);
+// }, {
+// 	scheduled: true,
+// 	timezone: "Asia/Yekaterinburg"
+// 	//timezone: "Europe/Moscow"
+//   });
 
 const sequelize = new Sequelize("mynotif", "root", "root", {
 	dialect: "mysql",
@@ -57,26 +164,29 @@ app.use(express.static(__dirname + "/public"));
  
 app.use(express.json());
 
-app.get("/load",function(request,response){
-	let currUserEmail = request.query.currUserEmail;
-	//console.log(currUserEmail);
-	if (currUserEmail){
-		//console.log(currUserEmail);
-		currUserEmail = currUserEmail.replace(/"/g,'');
-		console.log(currUserEmail);
-		if (currUserEmail){
-			PersBD.findAll({
-				attributes:['bdData'],
-				where:{
-					email:currUserEmail
-				}
-			}).then(res=>{
-				console.log("<<получены данные get запроса с параметром>>");
-				console.log(res);
-				response.json(res);
-			}).catch(err=>console.log(err));
-		}
-	}
+app.get("/load", function(request,response){
+
+	startCronTasks(cronTaskParamsArr);
+
+	// let currUserEmail = request.query.currUserEmail;
+	// //console.log(currUserEmail);
+	// if (currUserEmail){
+	// 	//console.log(currUserEmail);
+	// 	currUserEmail = currUserEmail.replace(/"/g,'');
+	// 	console.log(currUserEmail);
+	// 	if (currUserEmail){
+	// 		PersBD.findAll({
+	// 			attributes:['bdData'],
+	// 			where:{
+	// 				email:currUserEmail
+	// 			}
+	// 		}).then(res=>{
+	// 			console.log("<<получены данные get запроса с параметром>>");
+	// 			console.log(res);
+	// 			response.json(res);
+	// 		}).catch(err=>console.log(err));
+	// 	}
+	// }
 })
 
 app.get("/sel",function(request,response){
@@ -87,40 +197,44 @@ app.post("/", function (request, response) {
     //if(!request.body) return response.sendStatus(400);
 	//console.log(request.body.data)
 	if (request.body.data){
-		console.log(request.body.data);
-		const bdRows = JSON.stringify(request.body.data.bdRows);
-		let currUserEmail = JSON.stringify(request.body.data.currUserEmail);	
-		//console.log(currUserEmail);
-		if (currUserEmail){	
-			currUserEmail = currUserEmail.replace(/"/g,'');
-			PersBD.findAll({
-				where:{
-					email:currUserEmail
-				}
-			}).then(res=>{
-				console.log(res)
-				if (res.length>0){
-					console.log("<<изменение записи в базе>>")
-					PersBD.update({ bdData: bdRows }, {
-						where: {
-						email: currUserEmail
-						}
-					}).then((res) => {
-						console.log(res);
-					});
-				}else{
-					console.log("<<добавление записи в базу>>")
-					PersBD.create({
-						email: currUserEmail,
-						bdData: bdRows
-					}).then(res=>{
-						console.log(res);
-					}).catch(err=>console.log(err));
-				}
-				response.send("<<данные таблицы обновлены>>");
-			}).catch(err=>console.log(err));
-		}
-	}
+
+		stopCronTasks(cronTasks);
+
+	// 	console.log(request.body.data);
+	// 	const bdRows = JSON.stringify(request.body.data.bdRows);
+	// 	let currUserEmail = JSON.stringify(request.body.data.currUserEmail);	
+	// 	//console.log(currUserEmail);
+	// 	if (currUserEmail){	
+	// 		currUserEmail = currUserEmail.replace(/"/g,'');
+	// 		PersBD.findAll({
+	// 			where:{
+	// 				email:currUserEmail
+	// 			}
+	// 		}).then(res=>{
+	// 			console.log(res)
+	// 			if (res.length>0){
+	// 				console.log("<<изменение записи в базе>>")
+	// 				PersBD.update({ bdData: bdRows }, {
+	// 					where: {
+	// 					email: currUserEmail
+	// 					}
+	// 				}).then((res) => {
+	// 					console.log(res);
+	// 				});
+	// 			}else{
+	// 				console.log("<<добавление записи в базу>>")
+	// 				PersBD.create({
+	// 					email: currUserEmail,
+	// 					bdData: bdRows
+	// 				}).then(res=>{
+	// 					console.log(res);
+	// 				}).catch(err=>console.log(err));
+	// 			}
+	// 			response.send("<<данные таблицы обновлены>>");
+	// 		}).catch(err=>console.log(err));
+	// 	}
+	 }
+
     //response.send("!!!");
 });
 
